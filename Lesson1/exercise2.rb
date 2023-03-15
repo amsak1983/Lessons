170b32d3588ce12b91f4487bf4b7bcaeee09fb93
amsak1983 <amsak@yandex.by>
2023-03-15 19:24:00 +0300

    first commit

--- Lesson1/exercise2.rb
+++
@@ -1,8 +0,0 @@
-puts "Какая высота треугольника?"
-height = gets.chomp
-puts "Какой длинны основание?"
-base = gets.chomp
-a = base.to_i
-h = height.to_i
-puts "Площадь треугольника = #{a * h/2}"
-
--- Lesson4/Carriage.rb
+++
@@ -1,8 +0,0 @@
-class Carriage
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
-  end
-end
-
