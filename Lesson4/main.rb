7b6e54663af0e0cb4b7c673cc4235d4ff55940dd
amsak1983 <123478580+amsak1983@users.noreply.github.com>
2023-03-26 17:28:48 +0300

    Update main.rb

--- Lesson4/main.rb
+++ Lesson4/main.rb
@@ -1,3 +1,4 @@
+require_relative 'carriage'
 require_relative 'station'
 require_relative 'route'
 require_relative 'train'
@@ -5,7 +6,7 @@ require_relative 'passenger_train'
 require_relative 'passenger_carriage'
 require_relative 'cargo_train'
 require_relative 'cargo_carriage'
-require_relative 'carriage'
+
 require_relative 'menu'
 require_relative 'action'
 
