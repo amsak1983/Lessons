d7930c8615498146a165f68159b1cc81d8db9912
amsak1983 <amsak@yandex.by>
2023-05-10 18:11:57 +0300

    2 commit

--- Lesson7/carriage.rb
+++ Lesson7/carriage.rb
@@ -1,5 +1,22 @@
 class Carriage
   include Manufacturer
+  
   attr_reader :type
+  attr_reader :seats
+  attr_reader :passengers
+
+  def initialize(seats)
+    @seats = seats
+    register_instance
+    @passengers = 0
+  end
+
+  def take_seats
+    @passengers += 1 unless free_seats.zero?
+  end
+
+  def free_seats
+    @seats - @passengers
+  end
 end
 
--- Lesson7/passenger_carriage.rb
+++ Lesson7/passenger_carriage.rb
@@ -1,21 +1,8 @@
 class PassengerCarriage < Carriage
 
-  attr_reader :seats
-  attr_reader :passengers
-    
-  def initialize(seats)
-    @seats = seats
+  def initialize
     @type = :passenger
     register_instance
-    @passengers = 0
-  end
-
-  def take_seats
-    @passengers += 1 unless free_seats.zero?
-  end
-
-  def free_seats
-    @seats - @passengers
   end
 end
 
